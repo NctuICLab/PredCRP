294e21119bd864d9ed9014d17bbd84d1d709d11b
Rudy Tsai <rudy.tsai@dnarails.com>
2017-02-20 16:27:58 +0800

    Modified the input format

--- PredCRP.pl
+++ PredCRP.pl
@@ -6,7 +6,6 @@ use Cwd 'abs_path';
 use File::Basename;
 my $program = abs_path($0);
 my ($filename, $PredCRPdir) = fileparse($program);
-#print "$filename, $PredCRPdir\n";die;
 my $svmscale = $PredCRPdir.'/svm-scale';
 my $svmpredict = $PredCRPdir.'/svm-predict';
 my $model = $PredCRPdir.'/PredCRP_model';
@@ -17,13 +16,13 @@ my $input = '';
 my $help;
 sub Usage(){
 	print
-"Usage perl $program [Option]
+"Usage: perl $program [Option]
 Option:
-	-i FILE: input CRP binding sites information
-	-svmscale pathname: set svm-scale executable path and name
-	-svmpredict pathname: set svm-predict executable path and name
-	-model pathname: set PredCRP_model path and name
-	-h, -help
+-i FILE: input CRP binding sites information
+-svmscale pathname: set svm-scale executable path and name
+-svmpredict pathname: set svm-predict executable path and name
+-model pathname: set PredCRP_model path and name
+-h, -help
 ";
 }
 GetOptions(
@@ -42,21 +41,27 @@ if(!$input){
 	&Usage();
 	exit;
 }
+if($input !~ /\.csv/){
+	print STDERR "The format of csv must be csv\n";
+	exit;
+}
 my $svm_12features = fileparse($input);
-#print "$svm_12features\n";die;
+$svm_12features =~ s/\.csv//g;
 my $location_svm_12features = $tmp_location.'/'.$svm_12features."_svm";
 my $location_svm_12features_scl = $tmp_location.'/'.$svm_12features."_svm_scl";
 my $location_predict_result_tmp = $result_location.'/'.$svm_12features."_predict_tmp";
 my $location_predict_result = $result_location.'/'.$svm_12features."_predict";
-my $location_predict_final = $result_location.'/'.$svm_12features."_final";
+my $location_predict_final = $result_location.'/'.$svm_12features."_PredictResult.csv";
 my ($sequence, $promoter_flag, $bubble_flag, $ar_overlap, $ca_overlap);
 my ($aacg, $catt, $gaac, $gagc, $tgcg, $ttac, $ttat, $tttt);
+my $header = "CRPBS,Distance of Center Position of CRPBS to TSS,Transcription Unit,Regulatory Role";
 open SVM,">",$location_svm_12features;
 open FILE,"<",$input;
 my $line=<FILE>;
 chomp $line;
-if($line !~ /CRPBS<10bp_CRPBS22bp_10bp>/){
-	print STDERR "Format of input file is error\n";
+if($line !~ /$header/){
+	print "$line\n$header\n";
+	print STDERR "No header in input file\n";
 	exit;
 }
 my $num=0;
@@ -182,16 +187,16 @@ while($line=<FILE>){
 `$svmscale -r $scale $location_svm_12features > $location_svm_12features_scl`;
 `$svmpredict -b 1 $location_svm_12features_scl $model $location_predict_result_tmp`;
 open Pred,">",$location_predict_result;
-print Pred "Pred_role\tPred_probability\n";
+print Pred ",Pred role,Pred probability\n";
 open Predtmp,"<",$location_predict_result_tmp;
 $line=<Predtmp>;
 while($line=<Predtmp>){
 	chomp $line;
 	my @ele = split(/ /,$line);
 	if($ele[0] == 0){
-		print Pred "activation\t$ele[1]\n";
+		print Pred ",activation,$ele[1]\n";
 	}else{
-		print Pred "repression\t$ele[1]\n";
+		print Pred ",repression,$ele[1]\n";
 	}	
 }
 `paste $input $location_predict_result > $location_predict_final`;
